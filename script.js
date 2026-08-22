08406c07dae6bbdff5ef9dd82f6d4c1c3d508d06
Justin Lee <leejustincs@gmail.com>
2026-08-21 20:06:35 -0700

    feat: add JS function to load animation frames
    Signed-off-by: Justin Lee <leejustincs@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+async function loadFrames(animation) {
+  let path = `./sprite_sheet/monke_${animation}/monke_${animation}_sprite_sheet.json`;
+  let response = await fetch(path);
+
+  let status = response.status;
+  let data = await response.json();
+  console.log(status);
+  console.log(data);
+
+  return data;
+}
+
+let frames = await loadFrames("idle");
