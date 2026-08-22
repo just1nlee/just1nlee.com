672597781289164704a150444e4155eb8cb2a06b
Justin Lee <leejustincs@gmail.com>
2026-08-21 22:37:40 -0700

    feat: initial animation scripting
    Signed-off-by: Justin Lee <leejustincs@gmail.com>

--- script.js
+++ script.js
@@ -7,7 +7,34 @@ async function loadFrames(animation) {
   console.log(status);
   console.log(data);
 
-  return data;
+  return data.frames;
 }
 
+function displayFrame(frames, index) {
+  const element = document.querySelector(".monke");
+  const x = -frames[index].frame.x;
+  const y = -frames[index].frame.y;
+  element.style.backgroundPosition = `${x}px ${y}px`;
+}
+
+function runAnimation(frames) {
+  let numberOfFrames = frames.length;
+  let currentFrame = 0;
+
+  //  setTimeout(displayFrame(frames, currentFrame), 100);
+  for (let i = 0; i < numberOfFrames; i++) {
+    displayFrame(frames, i);
+  }
+}
+// function tick() {
+//   console.log("tick");
+//   setTimeout(tick, 100);
+// }
+// tick();
+
+// setTimeout(() => {
+//   console.log("this runs once, after waiting");
+// }, 10000);
+
 let frames = await loadFrames("idle");
+runAnimation(frames);
