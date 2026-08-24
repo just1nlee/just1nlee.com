a05b110dad7d5eca549a177a998982078d0653ab
Justin Lee <leejustincs@gmail.com>
2026-08-23 18:14:22 -0700

    feat: add initial monke click interation
    Signed-off-by: Justin Lee <leejustincs@gmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// const email_element = document.querySelector("#email");
+// email_element.addEventListener("click", (event) => {});
+
 async function loadFrames(animation) {
   let path = `./sprite_sheet/monke_${animation}/monke_${animation}_sprite_sheet.json`;
   let response = await fetch(path);
@@ -8,6 +11,12 @@ async function loadFrames(animation) {
   console.log(data);
 
   return data.frames;
+}
+
+function changeSpriteSheet(animation) {
+  let path = `./sprite_sheet/monke_${animation}/monke_${animation}_sprite_sheet.png`;
+  const element = document.querySelector(".monke");
+  element.style.backgroundImage = `url("${path}")`;
 }
 
 function displayFrame(frames, index) {
@@ -21,20 +30,32 @@ function runAnimation(frames) {
   let numberOfFrames = frames.length;
   let currentFrame = 0;
 
-  //  setTimeout(displayFrame(frames, currentFrame), 100);
   for (let i = 0; i < numberOfFrames; i++) {
-    displayFrame(frames, i);
+    console.log(`printing frame ${i}`);
+    setTimeout(displayFrame, 100 + 100 * i, frames, i);
   }
 }
-// function tick() {
-//   console.log("tick");
-//   setTimeout(tick, 100);
+
+async function emoteAnimation() {
+  let frames = await loadFrames("emote");
+  let monke = document.querySelector(".monke");
+  monke.addEventListener("click", (event) => {
+    for (let i = 0; i < frames.length; i++) {
+      setTimeout(displayFrame, 100 + 100 * i, frames, i);
+    }
+  });
+}
+
+changeSpriteSheet("emote");
+emoteAnimation();
+
+//let frames = await loadFrames("idle");
+//runAnimation(frames);
+
+// function copyEmail() {
+//   const element = document.querySelector("#email");
+//   element.addEventListener("click", (event) => {
+//     console.log("Email button clicked!");
+//   });
 // }
-// tick();
-
-// setTimeout(() => {
-//   console.log("this runs once, after waiting");
-// }, 10000);
-
-let frames = await loadFrames("idle");
-runAnimation(frames);
+// copyEmail();
